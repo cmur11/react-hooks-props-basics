6efe4472f851be6ded0651befc1b3c3009c733ff
Conor Murnane <conormurnane11@gmail.com>
2021-02-01 15:36:38 -0500

    commit tree 479d3c2dfdda557b6f6d2db518496d7111b971d5

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -9,13 +9,18 @@ function BlogPost() {
         articleText={
           "Dear Reader: Bjarne Stroustrup has the perfect lecture oration."
         }
+        isPublished={true}
+        minutesToRead={1}
+        articleName={"PostMalone"}
+        
       />
       <Comment commentText={"I agree with this statement. - Angela Merkel"} />
       <Comment commentText={"A universal truth. - Noam Chomsky"} />
       <Comment
         commentText={
-          "Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451"
-        }
+          "Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451"}/>
+      <Comment
+      commentText={ "not getting it"}
       />
     </div>
   );
